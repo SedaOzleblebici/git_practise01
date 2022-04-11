40e7ce876aabcb87247357780019ee2bf439551d
Seda <seda.ozleblebi@gmail.com>
2022-04-11 16:49:40 +0100

    work 3 is done and about to be pushed to github after commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,7 +4,11 @@ public class HelloWorld {
     // doıng some work here(you can type anything you want)
 
     // doing awesome work 2!
-    
+
+    //doing awesome work 3!
+    //awesome work 3 ıs done and ready to be commıtted
+    //and ready to be pushed to remote repository
+
 
 
 }
